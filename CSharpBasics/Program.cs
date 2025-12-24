44c056a9428d942fd0676e484728070961e60677
Loic S.R. <64268488+Sunbat@users.noreply.github.com>
2025-12-24 16:19:01 +0000

    FAFO1

---
+++ CSharpBasics/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+String greeting2 = "I'm Mr. Mee6, look at me!";
+Console.WriteLine(greeting2);
+// C# vs code problems
